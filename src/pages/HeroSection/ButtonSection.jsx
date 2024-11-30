7f4caa3afe04cf9f6ae03a1accd2d2370bad9b66
Mohamed Usama <usama@coducer.com>
2024-11-30 13:10:27 +0530

    fixed text ui and button

--- src/pages/HeroSection/ButtonSection.jsx
+++ src/pages/HeroSection/ButtonSection.jsx
@@ -3,7 +3,7 @@ import "./HeroSection.scss";
 
 const ButtonSection = () => {
   return (
-    <div className=" p-3 button-section">
+    <div className="button-section">
       <div className=" button-text">RESEARCH</div>
       {/* <div className=" button-border button-text">HOME</div> */}
       <div className="button-divider"></div>
