b6a6ebbe669b755235509ab189d59a1683c0908c
Mohamed Usama <usama@coducer.com>
2024-11-30 14:31:09 +0530

    fixed pdf viewer

--- src/pages/HeroSection/ButtonSection.js
+++ src/pages/HeroSection/ButtonSection.js
@@ -4,18 +4,7 @@ import pdfUrlLink from "../../assests/Darwin.pdf";
 
 const ButtonSection = () => {
   const handleResearchClick = () => {
-    // Open a new tab
-    const newTab = window.open("", "_blank");
-
-    // Embed the PDF inside the iframe in the new tab
-    newTab.document.title = "PDF Viewer";
-    newTab.document.body.innerHTML = `
-      <iframe src="${pdfUrlLink}#toolbar=0&navpanes=0&scrollbar=0&view=FitH" 
-              width="100%" 
-              height="100%" 
-              style="border: none;" 
-              allowfullscreen="true"></iframe>
-    `;
+    window.open(pdfUrlLink, "_blank");
   };
 
   const handleContactClick = () => {
@@ -26,12 +15,12 @@ const ButtonSection = () => {
       subject
     )}&body=${encodeURIComponent(body)}`;
   };
-
   return (
     <div className="button-section">
-      <div className="button-text" onClick={handleResearchClick}>
+      <div className=" button-text" onClick={handleResearchClick}>
         RESEARCH
       </div>
+      {/* <div className=" button-border button-text">HOME</div> */}
       <div className="button-divider"></div>
       <div className="button-text" onClick={handleContactClick}>
         CONTACT
