8606441e6a954ede00ed2dcd35fda379530eccbf
Mohamed Usama <usama@coducer.com>
2024-12-02 22:42:32 +0530

    added content

--- src/pages/HeroSection/HeroSection.js
+++ src/pages/HeroSection/HeroSection.js
@@ -24,67 +24,76 @@ const HeroSection = () => {
             </div>
             <div className="d-flex justify-content-center">
               <p className="sub-content-text">
-                At vero eos et accusamus et iusto odio dignissimos ducimus qui
-                blanditiis praesentium voluptatum deleniti atque corrupti quos
-                dolores et quas molestias excepturi sint occaecati cupiditate
-                non provident, similique sunt in culpa qui officia deserunt
-                mollitia animi, id est laborum et dolorum fuga. Et harum quidem
-                rerum facilis est et expedita distinctio. Nam libero tempore,
-                cum soluta nobis est eligendi optio cumque nihil impedit quo
-                minus id quod maxime placeat facere possimus, omnis voluptas
-                assumenda est, omnis dolor repellendus. Temporibus autem
-                quibusdam et aut officiis debitis aut rerum necessitatibus saepe
-                eveniet ut et voluptates repudiandae sint et molestiae non
-                recusandae. Itaque earum rerum hic tenetur a sapiente delectus,
-                ut aut reiciendis voluptatibus maiores alias consequatur aut
-                perferendis doloribus asperiores repellat.
+                Drug resistance remains a formidable challenge in cancer
+                treatment, with a range of implications such as variable drug
+                efficacy across patient populations (due to insufficient target
+                validation during drug discovery), unexpected side effects,
+                inconsistent therapeutic outcomes, and increased economic
+                burdens for pharmaceutical companies, hospitals, and patients.
               </p>
             </div>
             <div className="d-flex justify-content-center">
-              {" "}
               <p className="sub-content-text">
-                At vero eos et accusamus et iusto odio dignissimos ducimus qui
-                blanditiis praesentium voluptatum deleniti atque corrupti quos
-                dolores et quas molestias excepturi sint occaecati cupiditate
-                non provident, similique sunt in culpa qui officia deserunt
-                mollitia animi, id est laborum et dolorum fuga. Et harum quidem
-                rerum facilis est et expedita distinctio. Nam libero tempore,
-                cum soluta nobis est eligendi optio cumque nihil impedit quo
-                minus id quod maxime placeat facere possimus, omnis voluptas
-                assumenda est, omnis dolor repellendus. Temporibus autem
-                quibusdam et aut officiis debitis aut rerum necessitatibus saepe
-                eveniet ut et voluptates repudiandae sint et molestiae non
-                recusandae. Itaque earum rerum hic tenetur a sapiente delectus,
-                ut aut reiciendis voluptatibus maiores alias consequatur aut
-                perferendis doloribus asperiores repellat.
+                <b>OncoGalactica :</b> To investigate the molecular causes of
+                drug resistance in solid tumour conditions, Darwin (Bio + AI)
+                has been actively developing OncoGalactica, its flagship AI
+                Oncology Semantic framework, to:
+                <p className="sub-content-text pt-4">
+                  1. Integrate multi-dimensional molecular and clinical oncology
+                  data with AI semantics.
+                </p>
+                <p className="sub-content-text">
+                  2. Decipher causal relationships between genes and downstream
+                  receptor and signalling proteins.
+                </p>
+                <p className="sub-content-text">
+                  3. Reconstruct the unique molecular aetiology of individual
+                  patients.
+                </p>
+                <p className="sub-content-text">
+                  4. Identify key molecular mechanisms driving carcinogenesis
+                  and drug resistance in solid tumours with patient-level
+                  granularity.
+                </p>
+              </p>
+            </div>
+
+            <div className="d-flex justify-content-center">
+              <p className="sub-content-text">
+                <b>Clinical Validation Trial :</b> In a multi-centre clinical
+                validation trial conducted to validate the accuracy of
+                OncoGalactica in investigating the molecular causes of drug
+                resistance in advanced HER2-positive breast cancer patients who
+                had previously developed resistance to trastuzumab and taxanes,
+                11 patients in the treatment group, who were treated with an
+                OncoGalactica-recommended combination of pertuzumab and
+                alpelisib, showed an impressive Clinical Benefit Ratio (CBR) of
+                90.9% with 0.0% drug-related toxicities. This indicates the
+                accuracy of OncoGalactica in investigating the molecular causes
+                of drug resistance in solid tumour conditions with patient-level
+                granularity.
+              </p>
+            </div>
+            <div className="d-flex justify-content-center">
+              <p className="sub-content-text">
+                Please check the “Research” page below to get a high-level
+                understanding of our OncoGalactica R&D, use cases, and key
+                milestones.
+              </p>
+            </div>
+            <div className="d-flex justify-content-center">
+              <p className="sub-content-text">
+                If you wish to collaborate with us or are simply curious to know
+                more about our research objectives, please feel free to reach
+                out to us by clicking on “Contact”.
               </p>
             </div>
 
             <div className="d-flex justify-content-center">
               <p className="sub-content-text">
-                At vero eos et accusamus et iusto odio dignissimos ducimus qui
-                blanditiis praesentium voluptatum deleniti atque corrupti quos
-                dolores et quas molestias excepturi sint occaecati cupiditate
-                non provident, similique sunt in culpa qui officia deserunt
-                mollitia animi, id est laborum et dolorum fuga. Et harum quidem
-                rerum facilis est et expedita distinctio. Nam libero tempore,
-                cum soluta nobis est eligendi optio cumque nihil impedit quo
-                minus id quod maxime placeat facere possimus, omnis voluptas
-                assumenda est, omnis dolor repellendus. Temporibus autem
-                quibusdam et aut officiis debitis aut rerum necessitatibus saepe
-                eveniet ut et voluptates repudiandae sint et molestiae non
-                recusandae. Itaque earum rerum hic tenetur a sapiente delectus,
-                ut aut reiciendis voluptatibus maiores alias consequatur aut
-                perferendis doloribus asperiores repellat.
-              </p>
-            </div>
-
-            <div className="d-flex justify-content-center">
-              {" "}
-              <p className="sub-content-text">
-                <b className="sub-content-text ps-0">Privacy Notice:</b> We
-                prioritise user privacy and anonymity. Therefore, our digital
-                assets do not utilise cookies or user behaviour tracking tools.
+                <b>Privacy Notice :</b> We prioritise user privacy and
+                anonymity. Therefore, our digital assets do not utilise cookies
+                or user behaviour-tracking tools.
               </p>
             </div>
 
